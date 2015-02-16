0ee73f06ad6d3d2ad30133b651b278df0ffa9c4f
David Tay <davidt@aydus.com>
2015-02-16 01:02:33 -0500

    Added rwd handlers to nav filters

--- skin/frontend/base/default/js/aydus/quickaddtocart.js
+++ skin/frontend/base/default/js/aydus/quickaddtocart.js
@@ -6,7 +6,7 @@
  * @author     	Aydus Consulting <davidt@aydus.com>
  */
 
-var quickaddtocart = function ()
+var quickaddtocart = function ($)
 {
     //quickaddtocart controller
     var _controllerUrl;
@@ -83,10 +83,96 @@ var quickaddtocart = function ()
     	var addToCartText = res.translate['Add to Cart'];
     	jQuery('.category-products .actions, .category-products .action').find('a.button').attr('title',addToCartText).text(addToCartText).click(addToCart);
         
-        if (MenuManager){
-            MenuManager.init();
-        }
-        
+    	//from app.js
+    	if (enquire){
+    		
+            enquire.register('(max-width: ' + bp.medium + 'px)', {
+                setup: function () {
+                    this.toggleElements = $j(
+                        // This selects the menu on the My Account and CMS pages
+                        '.col-left-first .block:not(.block-layered-nav) .block-title, ' +
+                            '.col-left-first .block-layered-nav .block-subtitle--filter, ' +
+                            '.sidebar:not(.col-left-first) .block .block-title'
+                    );
+                },
+                match: function () {
+                    this.toggleElements.toggleSingle();
+                },
+                unmatch: function () {
+                    this.toggleElements.toggleSingle({destruct: true});
+                }
+            });
+            
+            $j('.toggle-content').each(function () {
+                var wrapper = jQuery(this);
+
+                var hasTabs = wrapper.hasClass('tabs');
+                var hasAccordion = wrapper.hasClass('accordion');
+                var startOpen = wrapper.hasClass('open');
+
+                var dl = wrapper.children('dl:first');
+                var dts = dl.children('dt');
+                var panes = dl.children('dd');
+                var groups = new Array(dts, panes);
+
+                //Create a ul for tabs if necessary.
+                if (hasTabs) {
+                    var ul = jQuery('<ul class="toggle-tabs"></ul>');
+                    dts.each(function () {
+                        var dt = jQuery(this);
+                        var li = jQuery('<li></li>');
+                        li.html(dt.html());
+                        ul.append(li);
+                    });
+                    ul.insertBefore(dl);
+                    var lis = ul.children();
+                    groups.push(lis);
+                }
+
+                //Add "last" classes.
+                var i;
+                for (i = 0; i < groups.length; i++) {
+                    groups[i].filter(':last').addClass('last');
+                }
+
+                function toggleClasses(clickedItem, group) {
+                    var index = group.index(clickedItem);
+                    var i;
+                    for (i = 0; i < groups.length; i++) {
+                        groups[i].removeClass('current');
+                        groups[i].eq(index).addClass('current');
+                    }
+                }
+
+                //Toggle on tab (dt) click.
+                dts.on('click', function (e) {
+                    //They clicked the current dt to close it. Restore the wrapper to unclicked state.
+                    if (jQuery(this).hasClass('current') && wrapper.hasClass('accordion-open')) {
+                        wrapper.removeClass('accordion-open');
+                    } else {
+                        //They're clicking something new. Reflect the explicit user interaction.
+                        wrapper.addClass('accordion-open');
+                    }
+                    toggleClasses(jQuery(this), dts);
+                });
+
+                //Toggle on tab (li) click.
+                if (hasTabs) {
+                    lis.on('click', function (e) {
+                        toggleClasses(jQuery(this), lis);
+                    });
+                    //Open the first tab.
+                    lis.eq(0).trigger('click');
+                }
+
+                //Open the first accordion if desired.
+                if (startOpen) {
+                    dts.eq(0).trigger('click');
+                }
+
+            });       		
+    	}
+     
     };
     
     //handle navigation selection
